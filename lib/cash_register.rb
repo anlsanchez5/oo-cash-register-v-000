48fd85377e4078b7a2e59054f38abccf14dad0db
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-04 07:55:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class CashRegister
-  attr_accessor :total, :discounts, :item
+  attr_accessor :total, :discounts, :items
 
 
   def initialize(discount = nil)
@@ -10,7 +10,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity = nil)
-    @item << item 
+    @items << item
     if quantity == nil
       self.total += price
     else
