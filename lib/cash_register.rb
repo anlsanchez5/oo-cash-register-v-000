dc132c70c28482509f0b4d2bf9b759c53434d253
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-04 07:38:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,4 +14,10 @@ class CashRegister
       self.total += (price*quantity)
     end
   end
+
+  def apply_discount
+    discount_amount = @total * (@discount/100)
+    final_amount = @total - discount_amount
+    puts "After the discount, the total comes to $#{final_amount}."
+  end
 end
