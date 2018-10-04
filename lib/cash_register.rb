1f0d6581207d05db64d6f20f3651f27f60b318cf
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-04 07:42:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,6 @@ class CashRegister
     discount_amount = @total * (@discount/100.0)
     self.total -= discount_amount
     puts "After the discount, the total comes to $#{@total}."
-binding.pry
+
   end
 end
